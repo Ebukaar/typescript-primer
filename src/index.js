108d4dfb8ea509f663b2f83777bded8877cb5bab
Ebukaar <Ebukaar@gmail.com>
2021-01-28 14:07:48 +0100

    My first commit for typescript

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var firstname = "Ebuka";
+var count = 10;
+firstname = "Paul";
+console.log(name);
+count = 5;
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let firstname: string = "Ebuka";
+
+let count: number = 10
+
+firstname = "Paul";
+
+console.log(name)
+
+count = 5
