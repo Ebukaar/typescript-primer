898716c2d067932cb2004e432a7fe3bbb6a1e8a9
Ebukaar <Ebukaar@gmail.com>
2021-02-04 13:04:11 +0100

    MY february commit

---
+++ build/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let firstname = "Ebuka";
+let count = 10;
+firstname = "Paul";
+console.log(firstname);
+count = 5;
+const secondname = 'Pius';
+console.log(secondname);
+const thirdname = `10`;
+console.log(thirdname);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import { getPriority } from "os";
+
 let firstname: string = "Ebuka";
 
 let count: number = 10
 
 firstname = "Paul";
 
-console.log(name)
+console.log(firstname)
 
 count = 5
+
+
+const secondname: string = 'Pius';
+    console.log(secondname)
+
+const thirdname: string = `10`;
+
+console.log(thirdname) 
